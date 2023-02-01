a2c89da4a7060f0c1505939e0e183c9e6550896c
Bohdan <Antoshkivbl@gmail.com>
2023-02-01 19:39:44 +0200

    rutes for auth

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
@@ -7,12 +8,33 @@ const contactsRouter = require("./routes/api/contacts");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+=======
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+require("dotenv").config();
+
+const authRouter = require('./routes/api/auth')
+const usersRouter = require("./routes/api/users");
+const contactsRouter = require('./routes/api/contacts')
+const app = express()
+>>>>>>> Stashed changes
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+<<<<<<< Updated upstream
 app.use("/api/contacts", contactsRouter);
+=======
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/auth', authRouter)
+app.use("/api/users", usersRouter);
+app.use('/api/contacts', contactsRouter)
+>>>>>>> Stashed changes
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
