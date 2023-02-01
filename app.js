52cd08d7d616f221fce4d821f54600fb7a8123bd
Bohdan <Antoshkivbl@gmail.com>
2023-02-01 22:56:53 +0200

    finish hw-4

--- app.js
+++ app.js
@@ -8,13 +8,11 @@ const usersRouter = require("./routes/api/users");
 const contactsRouter = require('./routes/api/contacts')
 const app = express()
 
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
 
 app.use('/api/auth', authRouter)
 app.use("/api/users", usersRouter);
