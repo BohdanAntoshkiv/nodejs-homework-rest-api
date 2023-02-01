5098125ef9acc8395f70dd80e0251bcd73a30f73
Bohdan <Antoshkivbl@gmail.com>
2023-02-01 19:52:32 +0200

    add model of user

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-<<<<<<< Updated upstream
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
-
-const contactsRouter = require("./routes/api/contacts");
-const app = express();
-
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-=======
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
@@ -18,15 +7,11 @@ const authRouter = require('./routes/api/auth')
 const usersRouter = require("./routes/api/users");
 const contactsRouter = require('./routes/api/contacts')
 const app = express()
->>>>>>> Stashed changes
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< Updated upstream
-app.use("/api/contacts", contactsRouter);
-=======
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
@@ -34,7 +19,6 @@ app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use("/api/users", usersRouter);
 app.use('/api/contacts', contactsRouter)
->>>>>>> Stashed changes
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
