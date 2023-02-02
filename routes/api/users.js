5d8902a2c43b99a2058c06c10663640cdc4521c1
Bohdan <Antoshkivbl@gmail.com>
2023-02-02 20:20:13 +0200

    add controler updateAvatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const { ctrlWrapper, authWrapper } = require("../../middlewares");
+const { ctrlWrapper, upload, authWrapper } = require("../../middlewares");
 const { users: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
 router.get("/current", authWrapper, ctrlWrapper(ctrl.getCurrent));
+router.patch("/avatars", upload.single("avatar"), authWrapper, ctrlWrapper(ctrl.updateAvatar));
 
 module.exports = router;
